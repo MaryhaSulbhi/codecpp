40c8cc257930a26c232c18c21318f34d35f117ad
MaryhaSulbhi <maryhasulbhi67@gmail.com>
2025-05-26 15:32:19 +0530

    poi

--- code.cpp
+++ code.cpp
@@ -344,7 +344,7 @@
 // using namespace std;
 // int main()
 // {
-//   int count=1;
+//   int count=1;  
 //   while(count <=5)
 //   {
 //     cout<<count<< " ";
@@ -382,7 +382,7 @@
 // }
 
 
-// ###########sum of num from 1 to n###########
+// ###########sum of num from 1 to n  ###########
 
 // #include<iostream>
 // using namespace std;
@@ -623,19 +623,20 @@
 // }
 
 // ###########Reverse/ backward Right Half Triangle ##########
-#include <iostream>
-using namespace std;
-int main()
-{
-  int n=5;
-  for(int i=n; i>0; i--){
-   cout<<i<<endl;
-  }
-  for(int i=0;i<n; i++){
-    for(int j=i+1; j>0; j--){
-      cout<<j<<" ";
-    }
-    cout<<endl;
-  }
-  return 0;
-}
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int n=5;
+//   for(int i=n; i>0; i--){
+//    cout<<i<<endl;
+//   }
+//   for(int i=0;i<n; i++){
+//     for(int j=i+1; j>0; j--){
+//       cout<<j<<" ";
+//     }
+//     cout<<endl;
+//   }
+//   return 0;
+// }
+
